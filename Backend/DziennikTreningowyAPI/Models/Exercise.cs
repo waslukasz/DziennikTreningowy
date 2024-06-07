3a296da9263d0910e60baf8c56548a44ad87f592
Łukasz Wąś <ljwasiu@gmail.com>
2024-06-07 09:36:56 +0200

    Initial backend commit

---
+++ Backend/DziennikTreningowyAPI/Models/Exercise.cs
@@ -0,0 +1,14 @@
+﻿namespace DziennikTreningowyAPI.Models;
+
+public class Exercise
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int? Weight { get; set; }
+    public int? Repetitions { get; set; }
+    public TimeSpan? Length { get; set; }
+    public int? TrainingId { get; set; }
+    public Training? Training { get; set; }
+    public DateTime Timestamp { get; set; }
+}
---
+++ Backend/DziennikTreningowyAPI/Models/Training.cs
@@ -0,0 +1,8 @@
+﻿namespace DziennikTreningowyAPI.Models;
+
+public class Training
+{
+    public int Id { get; set; }
+    public List<Exercise> Exercises { get; set; } = new List<Exercise>();
+    public DateTime Timestamp { get; set; }
+}

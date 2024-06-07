86273fcc2163800308b375f2fa53a82d2329a09a
Łukasz Wąś <ljwasiu@gmail.com>
2024-06-07 10:17:14 +0200

    Added Database context

---
+++ Backend/DziennikTreningowyAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DziennikTreningowyAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DziennikTreningowyAPI.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+        
+    }
+
+    public DbSet<Training> Trainings { get; set; }
+    public DbSet<Exercise> Exercises { get; set; }
+}

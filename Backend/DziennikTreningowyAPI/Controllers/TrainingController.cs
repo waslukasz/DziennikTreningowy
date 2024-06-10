a7ede896512e9049913781b4afa2c03ef6b90c84
Łukasz Wąś <ljwasiu@gmail.com>
2024-06-10 10:24:53 +0200

    Project now returns populated Exercises

--- Backend/DziennikTreningowyAPI/Controllers/TrainingController.cs
+++ Backend/DziennikTreningowyAPI/Controllers/TrainingController.cs
@@ -4,6 +4,7 @@ using DziennikTreningowyAPI.Dtos.Exercise;
 using DziennikTreningowyAPI.Dtos.Training;
 using DziennikTreningowyAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DziennikTreningowyAPI.Controllers;
 
@@ -23,16 +24,14 @@ public class TrainingController : Controller
     [HttpGet]
     public IActionResult GetAll()
     {
-        // TODO: Add select(exercises) and map them to ExerciseDtos
-        List<TrainingDto> trainings = _mapper.Map<List<Training>, List<TrainingDto>>(_context.Trainings.ToList());
+        List<TrainingDto> trainings = _mapper.Map<List<Training>, List<TrainingDto>>(_context.Trainings.Include((t) => t.Exercises).ToList());
         return Ok(trainings);
     }
 
     [HttpGet("{id}")]
     public IActionResult GetById([FromRoute] int id)
     {
-        // TODO: Add select(exercises) and map to ExerciseDto
-        TrainingDto? training = _mapper.Map<Training?, TrainingDto?>(_context.Trainings.Find(id));
+        TrainingDto? training = _mapper.Map<Training?, TrainingDto?>(_context.Trainings.Include((t) => t.Exercises).FirstOrDefault((t) => t.Id == id));
         if (training == null) return NotFound();
         return Ok(training);
     }
--- Backend/DziennikTreningowyAPI/obj/Debug/net8.0/DziennikTreningowyAPI.AssemblyInfo.cs
+++ Backend/DziennikTreningowyAPI/obj/Debug/net8.0/DziennikTreningowyAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DziennikTreningowyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b49fe3b52bf0cb165a2a5908d54577323bad500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f83aeee3122ec020fcc347dceb4b32288059e28")]
 [assembly: System.Reflection.AssemblyProductAttribute("DziennikTreningowyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DziennikTreningowyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

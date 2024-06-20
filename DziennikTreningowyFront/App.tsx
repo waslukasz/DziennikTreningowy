61c8d3c08a3cfea9a801f90bd0edd7b6469012e9
Mich <michuu1323@gmail.com>
2024-06-20 12:17:43 +0200

    initial

---
+++ DziennikTreningowyFront/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text>Dziennik Treningowy!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+

ef5d6216e6696356caf1ce7184b842158bdbf145
mttti <mateusz.magiera010@gmail.com>
2024-10-12 14:39:30 +0200

    added tailwind to MenuScreen

--- DziennikTreningowyFront/src/screens/MenuScreen.tsx
+++ DziennikTreningowyFront/src/screens/MenuScreen.tsx
@@ -3,18 +3,18 @@ import { View, TouchableOpacity, StyleSheet } from "react-native";
 
 const MenuScreen = ({ navigation }: { navigation: any }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.tile}>
+    <View className="p-2.5 flex flex-row flex-wrap justify-around">
+      <View className="border w-32 h-32 bg-white rounded-lg border-gray-300 mb-5">
         <TouchableOpacity
-          style={styles.button}
+          className="w-32 h-32 flex justify-center items-center"
           onPress={() => navigation.navigate("Training")}
         >
           <FontAwesome6 name="dumbbell" size={54} color="black" />
         </TouchableOpacity>
       </View>
-      <View style={styles.tile}>
+      <View className="border w-32 h-32 bg-white rounded-lg border-gray-300 mb-5">
         <TouchableOpacity
-          style={styles.button}
+          className="w-32 h-32 flex justify-center items-center"
           onPress={() => navigation.navigate("BodyMeasurment")}
         >
           <FontAwesome6 name="weight-scale" size={54} color="black" />
@@ -24,29 +24,3 @@ const MenuScreen = ({ navigation }: { navigation: any }) => {
   );
 };
 export default MenuScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-  },
-  tile: {
-    borderWidth: 1,
-    width: 125,
-    height: 125,
-    backgroundColor: "#fff",
-    borderRadius: 15,
-    borderColor: "#cbcbcb",
-    marginBottom: 20,
-  },
-  button: {
-    width: 125,
-    height: 125,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});

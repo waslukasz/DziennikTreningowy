0e0e87dced73fbea68a58ad0ed28f8f4650cffcb
mttti <mateusz.magiera010@gmail.com>
2024-10-19 11:42:57 +0200

    Added chart point info

--- DziennikTreningowyFront/src/components/measurement/Chart.tsx
+++ DziennikTreningowyFront/src/components/measurement/Chart.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-native-chart-kit";
 
 import { Dimensions } from "react-native";
+import { useState } from "react";
 
 export default function Chart({
   data,
@@ -17,9 +18,11 @@ export default function Chart({
   data: chartDataType[];
   name: string;
 }) {
+  const [selectedPoint, setSelectedPoint] = useState<string>();
   return (
     <View className="p-4">
       <Text className="text-center text-xl">{name}</Text>
+      <Text className="text-center text-xl">{selectedPoint}</Text>
       <LineChart
         data={{
           labels: data.map((item) => item.date),
@@ -29,9 +32,13 @@ export default function Chart({
             },
           ],
         }}
+        onDataPointClick={({ index }) => {
+          let point = data.map((item) => item.circumference)[index].toString();
+          setSelectedPoint(`${point} ${name == "Body Weight" ? "kg" : "cm"}`);
+        }}
         width={Dimensions.get("window").width - 20}
         height={300}
-        yAxisSuffix={name == "Body Weight" ? "kg" : "cm"}
+        yAxisSuffix={name == "Body Weight" ? " kg" : " cm"}
         yAxisInterval={1}
         xLabelsOffset={1}
         fromZero

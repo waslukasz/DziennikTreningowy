54b805fbe84c1e2c4ae4612a81c769f30d1876e8
waslukasz <lucas.was@protonmail.com>
2025-01-29 13:27:38 +0100

    Minor fixes.

--- DziennikTreningowyAPI/Application/DTOs/Measurement/MeasurementDetailsDto.cs
+++ DziennikTreningowyAPI/Application/DTOs/Measurement/MeasurementDetailsDto.cs
@@ -4,7 +4,7 @@ public class MeasurementDetailsDto
 {
     public Guid Id { get; set; }
     public Guid ProfileId { get; set; }
-    public string Type { get; set; }
+    public string BodyPart { get; set; }
     public double Value { get; set; }
     public DateTime MeasuredAt { get; set; }
     public DateTime UpdatedAt { get; set; }
--- DziennikTreningowyAPI/obj/Debug/net8.0/DziennikTreningowyAPI.AssemblyInfo.cs
+++ DziennikTreningowyAPI/obj/Debug/net8.0/DziennikTreningowyAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DziennikTreningowyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d0a30ae18f27b955983cab1fb37b3db0114bb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56d11df7f28eca0eac92b25cad4c22f63100af17")]
 [assembly: System.Reflection.AssemblyProductAttribute("DziennikTreningowyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DziennikTreningowyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
